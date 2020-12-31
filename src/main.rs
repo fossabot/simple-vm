e53a569d13c7d44ed2f8472fa38974b02d83891a
Renjith <grenjithr@gmail.com>
2020-12-31 00:42:13 -0600

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,27 +7,4 @@ use crate::instruction::*;
 use crate::memory::Memory;
 
 fn main() {
-    let mut memory = Memory::new(10);
-    memory.set_memory(0, MOV_LIT_REG);
-    memory.set_memory(1, 0x12);
-    memory.set_memory(2, 0x34);
-
-    memory.set_memory(3, MOV_LIT_R2);
-    memory.set_memory(4, 0xAB);
-    memory.set_memory(5, 0xCD);
-
-    memory.set_memory(6, ADD_REG_REG);
-    memory.set_memory(7, 0x01);
-    memory.set_memory(8, 0x02);
-
-    let mut cpu = CPU::new(memory);
-
-    cpu.debug();
-
-    cpu.step();
-    cpu.debug();
-    cpu.step();
-    cpu.debug();
-    cpu.step();
-    cpu.debug();
 }
